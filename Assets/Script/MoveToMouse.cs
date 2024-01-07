044899149f65beb9dd95a6e687f89c8485123466
Elllias <ellias_vo312@mail.ru>
2024-01-07 14:51:09 +0500

    Initial Commit

---
+++ Assets/Script/MoveToMouse.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Script
+{
+    [System.Serializable]
+    public class MoveToMouse : MonoBehaviour
+    {
+        public float Speed = 8.0f;
+        public float DistanceFromCamera = 5.0f;
+
+        private void Update()
+        {
+            var mousePosition = Input.mousePosition;
+        
+            mousePosition.z = DistanceFromCamera;
+
+            var mouseScreenToWorld = Camera.main.ScreenToWorldPoint(mousePosition);
+            var position = Vector3.Lerp(transform.position, mouseScreenToWorld,
+                1.0f - Mathf.Exp(-Speed * Time.deltaTime));
+
+            transform.position = position;
+        }
+    }
+}
---
+++ Assets/Script/Particles.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Script
+{
+    public class Particles : MonoBehaviour
+    {
+        private ParticleSystemRenderer _particleSystemRenderer;
+        private float _inputX;
+    
+        private void Start()
+        {
+            _particleSystemRenderer = GetComponent<ParticleSystemRenderer>();
+        }
+
+        private void Update()
+        {
+            _inputX = Input.GetAxis("Horizontal");
+
+            if (_inputX > 0)
+                _particleSystemRenderer.flip = new Vector3(1, 0, 0);
+            else if (_inputX < 0)
+                _particleSystemRenderer.flip = new Vector3(0, 0, 0);
+        }
+    }
+}
